2324a239e7fa2260b1d14cc432e33aeda57d4f3c
Agnieszka Drozd <yaa.eskaa@gmail.com>
2016-12-10 23:02:27 +0100

    Add files via upload
    *Initiate Board, Initiate Snake
    
    AD

---
+++ Snake/Snake/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly int SIZE = 10;
+        private SnakeObject _snake;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        void InitBoard()
+        {
+            for (int i = 0; i < grid.Width / SIZE; i++)
+            {
+                ColumnDefinition columnDefinitions = new ColumnDefinition();
+                columnDefinitions.Width = new GridLength(SIZE);
+                grid.ColumnDefinitions.Add(columnDefinitions);
+            }
+
+            for (int j = 0; j < grid.Height / SIZE; j++)
+            {
+                RowDefinition rowDefinitions = new RowDefinition();
+                rowDefinitions.Height = new GridLength(SIZE);
+                grid.RowDefinitions.Add(rowDefinitions);
+
+            }
+
+            _snake = new SnakeObject();
+
+        }
+
+        void initSnake()
+        {
+            grid.Children.Add(_snake.Head.Rect);
+            foreach (SnakeFragment snakeFragment in _snake.Fragments)
+                grid.Children.Add(snakeFragment.Rect);
+            _snake.DrawSnake();
+        }
+    }
+}
---
+++ Snake/Snake/SnakeObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Snake
+{
+    class SnakeObject
+    {
+        public SnakeFragment Head { get; private set; }
+        public List<SnakeFragment> Fragments { get; private set; }
+
+        public SnakeObject()
+        {
+            Head = new SnakeFragment(20, 0);
+            Head.Rect.Width = Head.Rect.Height = 10;
+            Head.Rect.Fill = System.Windows.Media.Brushes.Black;
+
+            Fragments = new List<SnakeFragment>();
+            Fragments.Add(new SnakeFragment(19, 0));
+            Fragments.Add(new SnakeFragment(18, 0));
+            Fragments.Add(new SnakeFragment(17, 0));
+            Fragments.Add(new SnakeFragment(16, 0));
+            Fragments.Add(new SnakeFragment(15, 0));
+            Fragments.Add(new SnakeFragment(14, 0));
+            Fragments.Add(new SnakeFragment(13, 0));
+            Fragments.Add(new SnakeFragment(12, 0));
+            Fragments.Add(new SnakeFragment(11, 0));
+            Fragments.Add(new SnakeFragment(10, 0));
+        }
+
+        public void DrawSnake()
+        {
+            Grid.SetColumn(Head.Rect, Head.X);
+            Grid.SetRow(Head.Rect, Head.Y);
+
+            foreach (SnakeFragment snakeFragment in Fragments)
+            {
+                Grid.SetColumn(snakeFragment.Rect, snakeFragment.X);
+                Grid.SetRow(snakeFragment.Rect, snakeFragment.Y);
+            }
+
+        }
+
+
+    }
+}
