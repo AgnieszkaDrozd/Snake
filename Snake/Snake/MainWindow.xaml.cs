01a29bb02f264851b7b8d383f3b14c388e2d329d
Agnieszka Drozd <yaa.eskaa@gmail.com>
2016-12-11 13:29:04 +0100

    Add files via upload
    *SnakeMove
    
    AD

--- Snake/Snake/MainWindow.xaml.cs
+++ Snake/Snake/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Snake
 {
@@ -22,6 +23,9 @@ namespace Snake
     {
         private static readonly int SIZE = 10;
         private SnakeObject _snake;
+        private int _directionX = 1;
+        private int _directionY = 0;
+        private DispatcherTimer _timer;
 
 
         public MainWindow()
@@ -30,6 +34,7 @@ namespace Snake
 
             initBoard();
             initSnake();
+            InitTimer();
         }
 
         void initBoard()
@@ -60,5 +65,63 @@ namespace Snake
                 grid.Children.Add(snakeFragment.Rect);
             _snake.DrawSnake();
         }
+
+        private void SnakeMove()
+        {
+            for (int i = _snake.Fragments.Count - 1; i >= 1; i--)
+            {
+                _snake.Fragments[i].X = _snake.Fragments[i - 1].X;
+                _snake.Fragments[i].Y = _snake.Fragments[i - 1].Y;
+            }
+
+            _snake.Fragments[0].X = _snake.Head.X;
+            _snake.Fragments[0].Y = _snake.Head.Y;
+            _snake.Head.X += _directionX;
+            _snake.Head.Y += _directionY;
+            _snake.DrawSnake();
+
+        }
+
+        void InitTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Tick += new EventHandler(_timer_Tick);
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            _timer.Start();
+
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            SnakeMove();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                _directionX = -1;
+                _directionY = 0;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                _directionX = 1;
+                _directionY = 0;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                _directionX = 0;
+                _directionY =-10;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                _directionX = 0;
+                _directionY = 1;
+            }
+        }
+
     }
 }
